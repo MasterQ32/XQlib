c9036ededf58196fac58adeee479b69f0ccc1832
Felix Queißner <git@mq32.de>
2019-02-07 09:27:14 +0100

    Improves fixed type by adding bitwise operators and more arithmetic.

--- src/xstd_fixed.cpp
+++ src/xstd_fixed.cpp
@@ -5,11 +5,33 @@ using var = xstd::fixed<22, 10>;
 static_assert(sizeof(var) == 4);
 static_assert(std::is_same_v<var::word_t, int32_t>);
 
+static_assert(var(10.0)  == var(10));
+static_assert(var(10U)   == var(10));
+static_assert(var(10.0f) == var(10));
+
+static_assert(-var(10) == var(-10));
+
 static_assert(var(10) + var(7.5) == var(17.5));
 static_assert(var(10) - var(7.5) == var(2.5));
 
 static_assert(var(10) / var(3) == var(3.3333333333333));
 static_assert(var(10) * var(3) == var(30.0));
+
+static_assert(var(10) % var(4) == var(2));
+static_assert(var(10.25) % var(0.5) == var(0.25));
+static_assert(var(-10) % var(4) == var(-2));
+static_assert(var(-10.25) % var(0.5) == var(-0.25));
+
+static_assert((var(2)  & var(8)) == var(0));
+static_assert((var(10) & var(8)) == var(8));
+static_assert((var(2)  | var(8)) == var(10));
+static_assert((var(10) | var(8)) == var(10));
+static_assert((var(2)  ^ var(8)) == var(10));
+static_assert((var(10) ^ var(8)) == var(2));
+static_assert((~var(10)) == var::raw(int32_t(0xFFFFD7FF)));
+
+// one day, we can bitcast here!
+// static_assert((var(10) >> 2) == var(2.5));
 
 static_assert(xstd::floor(var(3.0)) == var(3));
 static_assert(xstd::floor(var(3.4)) == var(3));
